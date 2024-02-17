5698370aebf70f49d12e69032899922da3eb8add
babaquero07 <brayanalexanderb@gmail.com>
2024-02-17 18:13:40 -0500

    refactor(api-users): added validator for user controller

--- api/src/app/user/user.controller.ts
+++ api/src/app/user/user.controller.ts
@@ -1,33 +1,37 @@
 import { Request, Response, Router } from "express";
 import { UserService } from "./user.service";
 
+import { validate, signupValidator } from "../../utils/validators";
+
 const userService = new UserService();
 
 const userRouter = Router();
 
-userRouter.post("/sign-up", async (req: Request, res: Response) => {
-  try {
-    const { name, user, password } = req.body;
+userRouter.post(
+  "/sign-up",
+  validate(signupValidator),
+  async (req: Request, res: Response) => {
+    try {
+      const { name, user, password } = req.body;
 
-    if (!name || !user || !password) {
-      return res.status(400).json({ message: "Missing fields" });
+      const existingUser = await userService.isUserRegister(user);
+
+      if (existingUser)
+        return res.status(422).send({ message: "User already exists" });
+
+      const newUser = await userService.signUp(name, user, password);
+
+      return res
+        .status(201)
+        .send({ ok: true, message: "use created", newUser });
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .send({ ok: false, message: "Internal server error" });
     }
-
-    const existingUser = await userService.isUserRegister(user);
-
-    if (existingUser)
-      return res.status(422).send({ message: "User already exists" });
-
-    const newUser = await userService.signUp(name, user, password);
-
-    return res.status(201).send({ ok: true, message: "use created", newUser });
-  } catch (error) {
-    console.log(error);
-
-    return res
-      .status(500)
-      .send({ ok: false, message: "Internal server error" });
   }
-});
+);
 
 export default userRouter;
---
+++ api/src/utils/validators.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { body, ValidationChain, validationResult } from "express-validator";
+
+export const validate = (validations: ValidationChain[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    for (let validation of validations) {
+      const result = await validation.run(req);
+
+      if (!result.isEmpty()) {
+        break;
+      }
+    }
+
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    return res.status(422).json({ errors: errors.array() });
+  };
+};
+
+export const loginValidator = [
+  body("user").notEmpty().withMessage("user is required").trim(),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .trim(),
+];
+
+export const signupValidator = [
+  body("name").notEmpty().withMessage("Name is required").trim(),
+  ...loginValidator,
+];
