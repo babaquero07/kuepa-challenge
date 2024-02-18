346b8377e02a0aff07960d8ae217d300ecf4e5ea
babaquero07 <brayanalexanderb@gmail.com>
2024-02-17 22:51:57 -0500

    Refactor authentication pages

--- web/src/pages/auth/AuthLayout.tsx
+++ web/src/pages/auth/AuthLayout.tsx
@@ -1,4 +1,19 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useAuth } from "../../context/AuthContext";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth?.isLoggedIn) {
+      console.log(auth);
+      // return navigate("/lesson/1");
+    }
+  }, [auth]);
+
   return (
     <main className="flex justify-center">
       <div className="w-full sm:w-[350px] px-10">{children}</div>
